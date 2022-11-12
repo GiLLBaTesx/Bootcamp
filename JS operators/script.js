a12bf851c4713afcbd325c9ccab5fdd85d2fdc04
Jhon Vincent Colasi <106292961+GiLLBaTesx@users.noreply.github.com>
2022-11-12 22:12:15 +0800

    JS OPERATOR ACTIVITY

---
+++ JS operators/script.js
@@ -0,0 +1,50 @@
+console.log("connected");
+
+// ESSAY PART:
+// 1.What is the difference between a comparison operator and an assignment operator?
+//  A: comparison operators only evaluate the value between element but does not change or alters the value while assignment operators change the value depending on the operator used.
+
+// 2.What is a Boolean? What does it represent?
+// A: Boolean is a tru/false statement. 
+
+// 3.How is the += operator different than + operator?
+// A: += refers to changing the value permanently, while + only changes the value for that certain operation it is called.
+
+
+/*Code Part
+
+1.Set two variables equal to two different numbers. Use a comparison operator to compare these two variables. Change one of their values by using the +=, -=, *=, or /= operator. Then, compare their values again.*/
+/*
+2.Try out the <= and >= operators.
+
+3.Try to use the !== operator.*/
+let a = 12;
+let b = 44;
+
+// number 1
+d = (a += b);
+
+    console.log(d);
+
+d = (a -= b);
+
+    console.log(d);
+
+d = (a *= b);
+
+    console.log(d);
+
+// number 2
+
+d = (a <= b);
+
+    console.log(d);
+
+d = (a >= b);
+
+    console.log(d);
+
+// number 3
+d = (a !== b);
+
+    console.log(d);
